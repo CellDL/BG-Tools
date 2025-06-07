c3328c763645921a7f496fbf1f73f41f486069d4
David Brooks <d.brooks@auckland.ac.nz>
2025-06-07 21:41:31 +1200

    Add functions to create MathML for summing a sequence of symbols and to equate two terms.

--- bg2cellml/mathml/__init__.py
+++ bg2cellml/mathml/__init__.py
@@ -27,6 +27,27 @@ import lxml.etree as etree
 
 #===============================================================================
 
+def equal(term_0: str, term_1: str) -> str:
+#==========================================
+    return f'''<apply>
+    <eq/>
+    {term_0}
+    {term_1}
+</apply>
+'''
+
+def sum_variables(symbols: list[str]) -> str:
+#============================================
+    if len(symbols) == 0:
+        return '<cn>0.0</cn>'
+    return f'''<apply>
+    <plus/>
+    {'\n    '.join([f'<ci>{symbol}</ci>' for symbol in symbols])}
+</apply>
+'''
+
+#===============================================================================
+
 class MathML:
     def __init__(self, mathml: etree.Element):
         self.__mathml = mathml
