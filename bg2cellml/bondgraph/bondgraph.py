0e2f9ce0701c583e8bd2fe369cfcb526676b6cab
David Brooks <d.brooks@auckland.ac.nz>
2025-06-08 12:34:21 +1200

    We've changed how Values are created from Literals.

--- bg2cellml/bondgraph/bondgraph.py
+++ bg2cellml/bondgraph/bondgraph.py
@@ -93,7 +93,7 @@ class BondgraphElement(Labelled):
         for row in model.source.sparql_query(ELEMENT_VARIABLES.replace('%ELEMENT_URI%', self.uri)):
             if row[0] not in self.__variables:
                 raise ValueError(f'Element {self.uri} has unknown symbol {row[0]} for {self.__template.uri}')   # type: ignore
-            self.__variables[row[0]].set_value(Value(row[1]))
+            self.__variables[row[0]].set_value(Value.from_literal(row[1]))
         return self
 
     @property
