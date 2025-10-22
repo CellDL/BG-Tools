d220c28034375986464ba5650c4a5d87d86cf306
David Brooks <d.brooks@auckland.ac.nz>
2025-10-22 19:54:43 +1300

    Add helper script for releases.

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git status | grep -q "nothing to commit"
+dirty=($? != 0)
+if (( dirty )); then
+    git stash -u
+fi
+
+uv build --wheel
+
+git push origin
+git push origin v$1
+gh release create v$1 --verify-tag --title "Release $1" --notes ""
+gh release upload v$1 dist/bg2cellml-$1-py3-none-any.whl
+
+if (( dirty )); then
+    git stash pop --quiet
+fi
