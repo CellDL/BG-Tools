69c27fc11906f8fb58c3e0e39b068dc800feadb8
David Brooks <d.brooks@auckland.ac.nz>
2025-11-04 12:19:48 +1300

    Remove unused import.

--- bg2cellml/__main__.py
+++ bg2cellml/__main__.py
@@ -24,7 +24,7 @@ from pathlib import Path
 
 from bg2cellml.bondgraph import BondgraphModel, BondgraphModelSource
 from bg2cellml.cellml import CellMLModel
-from bg2cellml.utils import log, pretty_log, string_to_list, valid_cellml
+from bg2cellml.utils import log, pretty_log, valid_cellml
 from bg2cellml.version import __version__
 
 #===============================================================================
