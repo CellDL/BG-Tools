a7e8a1df508fe26e486427f1b1326440cfcded8a
David Brooks <d.brooks@auckland.ac.nz>
2025-12-09 10:20:29 +1300

    Fancy logging belongs to the CLI application, not the library package.

--- bg2cellml/utils/__init__.py
+++ bg2cellml/utils/__init__.py
@@ -18,15 +18,12 @@
 #
 #===============================================================================
 
-import os.path
-import tempfile
+import logging
 from typing import Any, Optional, TYPE_CHECKING
 
 #===============================================================================
 
 import lxml.etree as etree
-import structlog
-from structlog.dev import BRIGHT, GREEN, RESET_ALL
 
 #===============================================================================
 
@@ -40,28 +37,18 @@ LOCAL_MODEL_BASE = 'https://bg-rdf.org/models/local/'
 #===============================================================================
 #===============================================================================
 
-structlog.configure(
-    processors=[
-        structlog.processors.TimeStamper(fmt="ISO"),
-        structlog.processors.add_log_level,
-        structlog.stdlib.PositionalArgumentsFormatter(),
-        structlog.processors.StackInfoRenderer(),
-        structlog.dev.ConsoleRenderer(colors=True)
-    ]
-)
-
-log = structlog.get_logger()
+log = logging.getLogger()
 
 #===============================================================================
 #===============================================================================
 
 def bright() -> str:
 #===================
-    return BRIGHT
+    return ''
 
 def pretty_log(s: Any) -> str:
 #=============================
-    return f'{RESET_ALL}{GREEN}{str(s)}{RESET_ALL}{BRIGHT}'
+    return s
 
 def pretty_uri(uri: Optional['str|NamedNode']) -> str:
 #=====================================================
