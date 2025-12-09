632c3c6aaa5d9327ac41a748c2a0280544318917
David Brooks <d.brooks@auckland.ac.nz>
2025-12-10 09:05:25 +1300

    rdf: complete `oxigraph` wrapper and (temp) put all code into the `__init__` file.

--- bg2cellml/rdf/__init__.py
+++ bg2cellml/rdf/__init__.py
@@ -18,15 +18,13 @@
 #
 #===============================================================================
 
+from collections import namedtuple
 from pathlib import Path
-from typing import Optional, Self
+from typing import Any, Optional, Self
 
 #===============================================================================
 
-from ..rdfstore import RdfStore
-from ..rdfstore import BlankNode, Literal, NamedNode
-from ..rdfstore import blankNode, literal, namedNode
-from ..rdfstore import isBlankNode, isLiteral, isNamedNode
+import pyoxigraph as oxigraph
 
 #===============================================================================
 
@@ -34,38 +32,90 @@ from ..utils import log
 
 #===============================================================================
 
+BlankNode = oxigraph.BlankNode
+Literal = oxigraph.Literal
+NamedNode = oxigraph.NamedNode
+
+#===============================================================================
+
+def blankNode(value: Optional[str]=None) -> BlankNode:
+    return BlankNode(value)
+
+def literal(value: str|int|float|bool, datatype: Optional[NamedNode]=None) -> Literal:
+    return Literal(value, datatype=datatype)
+
+def namedNode(uri: str) -> NamedNode:
+    return NamedNode(uri)
+
+#===============================================================================
+
+def isBlankNode(node: Any) -> bool:
+    return isinstance(node, BlankNode)
+
+def isLiteral(node: Any) -> bool:
+    return isinstance(node, Literal)
+
+def isNamedNode(node: Any) -> bool:
+    return isinstance(node, NamedNode)
+
+#===============================================================================
+
+def uri_fragment(uri: NamedNode) -> str:
+    full_uri = uri.value
+    return full_uri.rsplit('#')[-1]
+
+#===============================================================================
+
 type ResultType = BlankNode | Literal | NamedNode | None
 type ResultRow = list[ResultType]
+
+Triple = namedtuple('Triple', 'subject, predicate, object')
 
 #===============================================================================
 
 class RDFGraph:
     def __init__(self, namespaces: Optional[dict[str, str]]=None):
-        self.__graph = RdfStore(bind_namespaces='none', store='Oxigraph')
-        if namespaces is not None:
-            for prefix, namespace in namespaces.items():
-                self.__graph.bind(prefix, namespace)
+        self.__graph = oxigraph.Store()
+        self.__namespaces = namespaces or {}
 
     @property
     def graph(self):
         return self.__graph
 
-    def __contains__(self, triple: tuple) -> bool:
-    #=============================================
-        return triple in self.__graph
+    def __contains__(self, triple: Triple) -> bool:
+    #==============================================
+        try:
+            self.__graph.quads_for_pattern(triple.subject, triple.predicate, triple.object).__next__()
+            return True
+        except StopIteration:
+            return False
 
-    def add(self, triple: tuple) -> Self:
-    #====================================
-        self.__graph.add(triple)
+    def add(self, triple: Triple) -> Self:
+    #=====================================
+        self.__graph.add(oxigraph.Quad(triple.subject, triple.predicate, triple.object))
         return self
 
     def curie(self, uri: NamedNode) -> str:
     #======================================
-        return uri.n3(self.__graph.namespace_manager)
+        full_uri = uri.value
+        for prefix, ns_uri in self.__namespaces.items():
+            if full_uri.startswith(ns_uri):
+                return f'{prefix}:{full_uri[len(ns_uri):]}'
+        return full_uri
 
     def merge(self, graph: 'RDFGraph'):
     #==================================
-        self.__graph += graph.__graph
+        self.__graph.extend(graph.__graph.quads_for_pattern(None, None, None))
+
+    def parse(self, source_uri: str|Path) -> bool:
+    #=============================================
+        try:
+            print(f'Loading {source_uri}')
+            self.__graph.load(path=source_uri, format=oxigraph.RdfFormat.TURTLE)
+            return True
+        except Exception as e:
+            log.error(f'{e}: {source_uri}')
+            return False
 
     def query(self, query: str) -> list[ResultRow]:
     #==============================================
@@ -76,28 +126,12 @@ class RDFGraph:
             log.info(f'Query: {query}')
             return []
 
-    def parse(self, source_uri: str|Path) -> bool:
-    #=============================================
-        try:
-            self.__graph.parse(location=str(source_uri), format='ox-turtle')
-            return True
-        except Exception as e:
-            log.error(str(e))
-            return False
-
-    def remove(self, triple: tuple) -> Self:
-    #=======================================
-        self.__graph.remove(triple)
-        return self
-
     def serialise(self, source_url: Optional[str]=None) -> str:
     #==========================================================
         if source_url is not None:
-            self.__graph.bind('', f'{source_url}#')
-        return self.__graph.serialize(format='turtle')
-
-    def triples(self, triple: tuple):
-    #================================
-        return self.__graph.triples(triple)
+            self.__namespaces[''] =f'{source_url}#'
+        bytes = self.__graph.dump(format=oxigraph.RdfFormat.TURTLE,
+                                  prefixes=self.__namespaces)
+        return bytes.decode('utf-8')    # pyright: ignore[reportOptionalMemberAccess]
 
 #===============================================================================
